2ae45f82278f003ff0a11681cf421f50fe1f5f94
2년 차 개발자 이재영입니다 <148510209+LeeJaeYoung0706@users.noreply.github.com>
2023-10-20 11:27:10 +0900

    Create Guard.tsx

---
+++ Guard.tsx
@@ -0,0 +1,44 @@
+import React, { ReactNode, useEffect } from "react";
+import { deleteUserToken } from "../../store/slices/userSlice";
+import { useNavigate } from "react-router-dom";
+import { useAppSelector, useAppDispatch } from "../../store/hooks";
+import { RootState } from "../../store";
+
+interface Props {
+    children: ReactNode;
+}
+
+type initialState = {
+    isToken: false;
+    isJoin: false;
+    isLogin: false;
+    token: "";
+};
+
+const UserGuard = ({ children }: Props) => {
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+    const { isJoin, isToken, isLogin, token }: initialState = useAppSelector((state: RootState) => state.userSlice);
+
+    useEffect(() => {
+        if (!isLogin) {
+            // 로그인 상태가 아닐 때 로그인 화면으로 이동
+            if ((!isToken || token === "") && !isJoin) {
+                dispatch(deleteUserToken());
+                window.location.href = `${process.env.REACT_APP_REST_URL}/auth/login`;
+            }
+            // 홈페이지 DB 엔 없고 KeyCloak 로그인 상태일 때 회원 가입
+            else if (!isJoin && isToken) {
+                navigate(`/new/user`, { replace: true });
+            // 이외의 상황일 때
+            } else {
+                dispatch(deleteUserToken());
+                navigate(`/`, { replace: true });
+            }
+        }
+    }, []);
+
+    return <>{children}</>;
+};
+
+export default UserGuard;
